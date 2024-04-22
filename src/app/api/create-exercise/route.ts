a85e57085a713d7877e310be3ddfc1b8fc219032
mhoang <mhoangsb@gmail.com>
2024-04-22 16:46:50 +0700

    Create draft api route to test database

--- src/app/api/create-exercise/route.ts
+++ src/app/api/create-exercise/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/client";
+import { cookies } from "next/headers";
 
 export async function GET() {
+  cookies();
   const createdExercise = await prisma.exercise.create({
     data: {
       question: "What's your name?",
