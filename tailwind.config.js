2c825e7bc388792849cfc387a12bb16e7010bc91
mhoang <mhoangsb@gmail.com>
2024-04-22 11:53:20 +0700

    Install TailwindCSS in this project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
